2f1ebed239ab09f988aef7ac8c6914fb01c933b7
Nitin Reddy <nitin.reddy@appdirect.com>
2022-01-17 22:48:32 +0530

    Removed the node_modules

--- index.ts
+++ index.ts
@@ -1,34 +1,43 @@
-const {MongoClient} = require('mongodb');
+// const {MongoClient} = require('mongodb');
 
-async function listDatabases(client){
-   let databasesList = await client.db().admin().listDatabases();
+// async function listDatabases(client){
+//    let databasesList = await client.db().admin().listDatabases();
  
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+//     console.log("Databases:");
+//     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+// };
 
-async function main(){
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-    const uri = "mongodb://localhost:37017/?readPreference=primaryPreferred&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+// async function main(){
+//     /**
+//      * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+//      * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+//      */
+//     const uri = "mongodb://localhost:37017/?readPreference=primaryPreferred&appname=MongoDB%20Compass&directConnection=true&ssl=false";
  
 
-    const client = new MongoClient(uri);
+//     const client = new MongoClient(uri);
  
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
+//     try {
+//         // Connect to the MongoDB cluster
+//         await client.connect();
  
-        // Make the appropriate DB calls
-        await  listDatabases(client);
+//         // Make the appropriate DB calls
+//         await  listDatabases(client);
  
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
+//     } catch (e) {
+//         console.error(e);
+//     } finally {
+//         await client.close();
+//     }
+// }
 
-main().catch(console.error);
+// main().catch(console.error);
+
+var {MongoClient} = require('mongodb');
+var url = "mongodb://localhost:37017/?readPreference=primaryPreferred&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
